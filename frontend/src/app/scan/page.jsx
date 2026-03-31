1756f254550b04829ab627ed513ffcae0292d62e
lioTshite <tshitengelionel@gmail.com>
2026-04-01 00:26:39 +0200

    creation d'une progressive web app

--- frontend/src/app/scan/page.jsx
+++ frontend/src/app/scan/page.jsx
@@ -83,7 +83,7 @@ export default function ScanPage() {
     const verifyToken = async (token) => {
         setLoading(true);
         try {
-            const res = await fetch("http://localhost:5000/qr/verify", {
+            const res = await fetch("https://tidy-teeth-turn.loca.lt/qr/verify", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ token }),
