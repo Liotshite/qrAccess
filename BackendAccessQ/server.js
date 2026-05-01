db10ece064cae5b75fe5b769035146229fd30922
lioTshite <tshitengelionel@gmail.com>
2026-05-01 13:53:33 +0200

    preparation pour render

--- BackendAccessQ/server.js
+++ BackendAccessQ/server.js
@@ -7,6 +7,6 @@ const app = require("./src/app");
 // ===== Lancer le serveur =====
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Serveur démarré sur le port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur démarré sur le port `);
 });
