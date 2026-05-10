1621b0c6204ebdc3b07191186b0b4b3ba8ebd7de
lioTshite <tshitengelionel@gmail.com>
2026-05-10 15:15:13 +0200

    changement de serveur smtp(Gmail)

--- BackendAccessQ/src/services/email.service.js
+++ BackendAccessQ/src/services/email.service.js
@@ -5,7 +5,7 @@ const createTransporter = async () => {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: Number(process.env.SMTP_PORT),
-        secure: process.env.SMTP_PORT === "465",
+        secure: false,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
